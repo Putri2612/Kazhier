9b979f957564f0dda10701538e5fd45c19498465
Levine Lighto <lighto.cc2@gmail.com>
2021-11-10 18:48:00 +0700

    Tambah Kategori default

--- app/app/Http/Controllers/Auth/RegisterController.php
+++ app/app/Http/Controllers/Auth/RegisterController.php
@@ -8,7 +8,6 @@ use App\Models\User;
 use App\Models\ProductServiceCategory;
 use App\Models\Tax;
 use App\Models\ProductServiceUnit;
-use App\Models\BankAccount;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -100,54 +99,133 @@ class RegisterController extends Controller
                 'plan' => 1,
             ]
         );
-        // Penjualan
-        $product                = new ProductServiceCategory();
-        $product->name          = 'Produk';
-        $product->color         = '4ffa00';
-        $product->type          = '0';
-        $product->created_by    = $user->creatorId();
-        $product->save();
+        // Kategori
+        ProductServiceCategory::insert([
+            // Barang & Jasa
+            [
+                'name'          => 'Produk',
+                'color'         => 'befba2',
+                'type'          => '0',
+                'created_by'    => $user->creatorId()
+            ], [
+                'name'          => 'Layanan jasa',
+                'color'         => '87b771',
+                'type'          => '0',
+                'created_by'    => $user->creatorId()
+            ],
+            
+            [
+                'name'          => 'Penjualan',
+                'color'         => '8ed1fb',
+                'type'          => '1',
+                'created_by'    => $user->creatorId()
+            ], [
+                'name'          => 'Penambahan modal',
+                'color'         => 'b2d8f0',
+                'type'          => '1',
+                'created_by'    => $user->creatorId()
+            ], [
+                'name'          => 'Pendapatan lain',
+                'color'         => 'adc4d2',
+                'type'          => '1',
+                'created_by'    => $user->creatorId()
+            ], [
+                'name'          => 'Pendapatan jasa',
+                'color'         => '648ca6',
+                'type'          => '1',
+                'created_by'    => $user->creatorId()
+            ], [
+                'name'          => 'Hibah',
+                'color'         => 'a5b2bb',
+                'type'          => '1',
+                'created_by'    => $user->creatorId()
+            ], [
+                'name'          => 'Pinjaman',
+                'color'         => '5989ab',
+                'type'          => '1',
+                'created_by'    => $user->creatorId()
+            ], [
+                'name'          => 'Piutang',
+                'color'         => '61a5d6',
+                'type'          => '1',
+                'created_by'    => $user->creatorId()
+            ], 
+            
+            [
+                'name'          => 'Pembelian',
+                'color'         => 'f0af8f',
+                'type'          => '2',
+                'created_by'    => $user->creatorId()
+            ], [
+                'name'          => 'Pembelian bahan baku',
+                'color'         => 'eea0a0',
+                'type'          => '2',
+                'created_by'    => $user->creatorId()
+            ], [
+                'name'          => 'Biaya operasional',
+                'color'         => 'f2cfa6',
+                'type'          => '2',
+                'created_by'    => $user->creatorId()
+            ], [
+                'name'          => 'Pengeluaran lain-lain',
+                'color'         => 'c9907e',
+                'type'          => '2',
+                'created_by'    => $user->creatorId()
+            ], [
+                'name'          => 'Pembayaran utang',
+                'color'         => 'e88e73',
+                'type'          => '2',
+                'created_by'    => $user->creatorId()
+            ], [
+                'name'          => 'Pemberian utang',
+                'color'         => 'd7865b',
+                'type'          => '2',
+                'created_by'    => $user->creatorId()
+            ],
+        ]);
         
-        // Penjualan
-        $product                = new ProductServiceCategory();
-        $product->name          = 'Penjualan';
-        $product->color         = '0097f8';
-        $product->type          = '1';
-        $product->created_by    = $user->creatorId();
-        $product->save();
-        
-        $product                = new ProductServiceCategory();
-        $product->name          = 'Pembelian';
-        $product->color         = 'ff5909';
-        $product->type          = '2';
-        $product->created_by    = $user->creatorId();
-        $product->save();
 
-        // 'pajak'
-        $product                = new Tax();
-        $product->name          = 'PPn';
-        $product->rate          = '10';
-        $product->created_by    = $user->creatorId();
-        $product->save();
+        // Pajak
+        Tax::insert([
+            [
+                'name'          => 'Bebas pajak',
+                'rate'          => '0',
+                'created_by'    => $user->creatorId()
+            ], [
+                'name'          => 'PPn',
+                'rate'          => '10',
+                'created_by'    => $user->creatorId()
+            ]
+        ]);
 
-        //'produk dan layanan'
-        $product                = new ProductServiceUnit();
-        $product->name          = 'unit';
-        $product->created_by    = $user->creatorId();
-        $product->save();
+        // Satuan
+        ProductServiceUnit::insert([
+            [ 'name'          => 'Botol',   'created_by'    => $user->creatorId() ],
+            [ 'name'          => 'Bungkus', 'created_by'    => $user->creatorId() ],
+            [ 'name'          => 'Copy',    'created_by'    => $user->creatorId() ],
+            [ 'name'          => 'Dus',     'created_by'    => $user->creatorId() ],
+            [ 'name'          => 'Gram',    'created_by'    => $user->creatorId() ],
+            [ 'name'          => 'Item',    'created_by'    => $user->creatorId() ],
+            [ 'name'          => 'Kaleng',  'created_by'    => $user->creatorId() ],
+            [ 'name'          => 'Karung',  'created_by'    => $user->creatorId() ],
+            [ 'name'          => 'kg',      'created_by'    => $user->creatorId() ],
+            [ 'name'          => 'Lembar',  'created_by'    => $user->creatorId() ],
+            [ 'name'          => 'Liter',   'created_by'    => $user->creatorId() ],
+            [ 'name'          => 'Ons',     'created_by'    => $user->creatorId() ],
+            [ 'name'          => 'Pasang',  'created_by'    => $user->creatorId() ],
+            [ 'name'          => 'Unit',    'created_by'    => $user->creatorId() ], 
+        ]);
 
-        //cara pembayaran
-        $product                = new PaymentMethod();
-        $product->name          = 'transfer bank';
-        $product->created_by    = $user->creatorId();
-        $product->save();
-       
-        $product                = new PaymentMethod();
-        $product->name          = 'cash';
-        $product->created_by    = $user->creatorId();
-        $product->save();
-
-        
+        // Cara pembayaran
+        PaymentMethod::insert([
+            [
+                'name'          => 'Cash',
+                'created_by'    => $user->creatorId()
+            ], [
+                'name'          => 'Transfer bank',
+                'created_by'    => $user->creatorId()
+            ],
+        ]);
 
         $role_r = Role::findByName('company');
 
