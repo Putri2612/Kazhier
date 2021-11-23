dd8bdab5d8332d547b22dfb93af08418aa001628
Levine Lighto <lighto.cc2@gmail.com>
2021-11-23 14:52:05 +0700

    redirect nanti pindah sini

---
+++ app/app/Traits/CanRedirect.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Traits;
+
+trait CanRedirect{
+    public function RedirectSuccess($message){
+        return redirect()->back()->with('success', __($message));
+    }
+
+    public function RedirectError($message = 'Something is wrong.'){
+        return redirect()->back()->with('error', __($message));
+    }
+
+    public function RedirectDenied(){
+        return $this->RedirectError('Permission denied.');
+    }
+}
