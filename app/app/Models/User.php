d58023e1cd4dcc8f937c819e849d0d6f5d090063
Levine Lighto <lighto.cc2@gmail.com>
2021-12-01 23:02:11 +0700

    aktivitas, ketinggalan

--- app/app/Models/User.php
+++ app/app/Models/User.php
@@ -39,6 +39,7 @@ class User extends Authenticatable
 
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'last_active'       => 'datetime',
     ];
 
     public function user(){
