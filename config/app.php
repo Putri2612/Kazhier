9db3de3b8ca10b86f12181494d7fd457537b9bb8
Levine Lighto <lighto.cc2@gmail.com>
2022-04-07 22:57:19 +0700

    hapus installer

--- config/app.php
+++ config/app.php
@@ -174,7 +174,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        RachidLaasri\LaravelInstaller\Providers\LaravelInstallerServiceProvider::class,
         Laravel\Passport\PassportServiceProvider::class,
         Maatwebsite\Excel\ExcelServiceProvider::class,
     ],
