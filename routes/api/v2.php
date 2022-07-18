ec795bcb06577164de551e51f80879abc155f4b1
Levine Lighto <lighto.cc2@gmail.com>
2022-07-18 20:37:07 +0700

    route terlupa

--- routes/api/v2.php
+++ routes/api/v2.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\api\v2\AssetController;
 use App\Http\Controllers\api\v2\BalanceSheetController;
+use App\Http\Controllers\api\v2\BankAccountController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => ['auth:api', 'plan']], function() {
@@ -11,6 +12,13 @@ Route::group(['middleware' => ['auth:api', 'plan']], function() {
         Route::post('create', [AssetController::class, 'create'])->name('create');
         Route::put('{id}/update', [AssetController::class, 'update'])->name('update');
         Route::delete('{id}/delete', [AssetController::class, 'destroy'])->name('delete');
+    });
+
+    Route::prefix('bank-account')->as('bank-account.')->group(function() {
+        Route::get('/', [BankAccountController::class, 'get'])->name('get');
+        Route::post('create', [BankAccountController::class, 'create'])->name('create');
+        Route::put('{id}/update', [BankAccountController::class, 'update'])->name('update');
+        Route::delete('{id}/delete', [BankAccountController::class, 'destroy'])->name('delete');
     });
 
     Route::prefix('report')->as('report.')->group(function() {
